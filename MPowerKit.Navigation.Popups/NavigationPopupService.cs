1422dbb6b0cc3b19d5fff42342118c63fb732345
Alex Dobrynin <shuron1988@gmail.com>
2025-07-07 14:29:11 +0200

    fixed popups for android

--- MPowerKit.Navigation.Popups/NavigationPopupService.cs
+++ MPowerKit.Navigation.Popups/NavigationPopupService.cs
@@ -5,10 +5,5 @@ namespace MPowerKit.Navigation.Popups;
 
 public partial class NavigationPopupService : PopupService, INavigationPopupService
 {
-#if ANDROID && NET9_0_OR_GREATER
-    protected override void BackPressCallback(PopupPage page, global::Android.App.Activity activity)
-    {
-        activity.OnBackPressed();
-    }
-#endif
+
 }
